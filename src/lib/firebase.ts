5807cd642c4aa54bccd41191869bab09348507b3
LuisZambranos <luisf.zambrano0810@gmail.com>
2025-12-17 10:09:05 -0300

    cambie el repo

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -20,4 +20,3 @@ const app = initializeApp(firebaseConfig);
 // Inicializar y exportar los servicios para usarlos en el resto de la app
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-

6c71cd949042f8bc5856964c613377003b955fc0
LuisZambranos <luisf.zambrano0810@gmail.com>
2026-04-11 09:24:08 -0400

    añadi la referencia directa para que se instale

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "next-themes";
 import App from "./App.tsx";
 import "./index.css";
+// 1. Importas la función de registro para la PWA
+import { registerSW } from 'virtual:pwa-register';
+
+// 2. Registras el Service Worker para habilitar la instalación
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
